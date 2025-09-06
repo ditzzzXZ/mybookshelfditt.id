6e3fca7d768e2882d50addd0d711e40acfca239a
ditzzzXZ <ditzzz.ofc@gmail.com>
2025-09-06 07:05:14 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#22c55e',
+        dark: '#1e293b',
+      },
+    },
+  },
+  plugins: [],
+}
